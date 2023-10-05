8c79b4a454768e927915aec50925ac7941a2dd4f
Amitai Blickstein <blickstein@gmail.com>
2023-10-05 13:11:17 -0400

    Initial Commit

---
+++ StaticAnalyzerTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StaticAnalyzerTest
+//
+//  Created by Amitai Blickstein on 10/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
